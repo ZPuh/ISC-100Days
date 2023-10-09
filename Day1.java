c7b23f92652763d6afef6a336f8a6bddfb52e3d8
Muhammad Zuhdi <146620601+ZPuh@users.noreply.github.com>
2023-10-09 16:03:59 +0800

    Add files via upload

---
+++ Day1.java
@@ -0,0 +1,49 @@
+package day.pkg1;
+public class Day1 {
+    public static void main(String[] args) {
+        /*Hy Kak,dihari pertama dikegiatan 100 hari ngoding 
+        Saya akan membuat content tentang Komentar dan cara
+        mencetak/menampilkan data pada java               
+        
+        komentar (comment) adalah blok code yang tidak akan 
+        dibaca dan tidak akan dieksekusi oleh computer.
+        Ada 2 macam komentar yaitu :
+        1.Komentar satu baris 
+            komentar ini hanya bisa ditulis dalam satu baris
+        saja.Penulisan komentar ini diawali dengan tanda (//) */
+        
+        //contoh :
+        //ini adalah komentar satu baris
+        
+        /* 
+        2. Komentar Banyak baris 
+            komentar ini bisa menuliskan satu atau beberapa baris 
+        komentar.Penulisan komentar ini dibuka dan ditutup dengan (/*)
+        Contoh :
+        Pada komentar ini menggunakan komentar banyak baris.
+        */
+        
+        //cara mencetak data
+        /*cara mencetak pada java menggunakan System.out.println("")
+        atau bisa juga dengan sout + tab
+        Contoh : */
+        // mencetak data bertipe String (kata/kalimat) 
+        System.out.println("Hy Nama Saya Muhammad Zuhdi");
+        // mencetak data bertipe Integer (angka)
+        System.out.println(18);
+        // mencetak data bertipe char (karakter)
+        System.out.println('L');
+        // mencetak data bertipe boolean (true/false)
+        System.out.println(false);
+        
+        // sekian dari saya mohon maaf kalau ada salah 
+        
+    }    
+}
+
+
+
+
+
+
+
