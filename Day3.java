97cc61d81f3de2f9ed4ee087894917f3dbac5382
Muhammad Zuhdi <146620601+ZPuh@users.noreply.github.com>
2023-10-11 14:52:52 +0800

    (3/100)
    Tipe data charakter dan boolean

---
+++ Day3.java
@@ -0,0 +1,32 @@
+package day3;
+public class Day3 {
+    public static void main(String[] args) {
+   /*Hy Kak,dihari ketiga dikegiatan 100 hari ngoding. Saya akan membuat content 
+     tipe data primitif.Hari ini saya akan  membahas tipe data Charakter dan
+     Boolean*/
+     // TIPE DATA CHARAKTER (CHAR)
+     // Untuk membuat data char, kita menggunakan tanda 'petik 1' diawal dan 
+     // akhir karakter
+        char charz = 'Z'; //char hanya bisa menampung 1 karakter
+        char charu = 'U';
+        char charh = 'H';
+        char chard = 'D';
+        char chari = 'I';
+        
+        System.out.println(charz);
+        System.out.println(charu);
+        System.out.println(charh);
+        System.out.println(chard);
+        System.out.println(chari);
+        
+     //TIPE DATA BOOLEAN 
+     //Tipe data boolean hanya mempunyai 2 nilai,yaitu true dan false
+        boolean benar = true;
+        boolean salah = false;
+        
+        System.out.println(benar);
+        System.out.println(salah);
+        
+     //Sekian untuk hari ini kak,kalau ada salah mohon dikoreksi.Sampai jumpa 
+     //hari esok   
+ } }  
