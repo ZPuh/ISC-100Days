eb3ba5a0fae16ed0f394317b043e5c1dd5f707bb
Muhammad Zuhdi <146620601+ZPuh@users.noreply.github.com>
2023-10-12 21:37:25 +0800

    (4/100)
    Tipe data non primitif

---
+++ Day4.java
@@ -0,0 +1,39 @@
+package day4;
+public class Day4 {
+    public static void main(String[] args) {
+    // TIPE DATA NON PRIMITIF
+    // String
+    String nama = "Muhammad Sumbul"; 
+    // Byte
+    Byte umur = 21;
+    // Short
+    Short angkatan = 2020;
+    // Integer
+    Integer nim = 70223320;
+    // Long
+    Long nik = 76050248395934657L;
+    // Float
+    Float bb = 96.13F;
+    // Double
+    Double tb = 297.13;
+    //Character
+    Character kelas = 'B';
+    // Bollean
+    Boolean iniboolean = false;  
+    System.out.println("=========BIODATA MABA=========");
+    System.out.println("===NAMA : "+nama+"  ===");
+    System.out.println("===UMUR : "+umur+"\t\t   ===");
+    System.out.println("===ANGK : "+angkatan+"\t\t   ===");
+    System.out.println("===NIM  : "+nim+"\t   ===");
+    System.out.println("===NIK  : "+nik+"===");
+    System.out.println("===BB   : "+bb+"\t\t   ===");
+    System.out.println("===TT   : "+tb+"\t   ===");
+    System.out.println("===KLS  : "+kelas+"\t\t   ===");
+    System.out.println("===JMLO : "+iniboolean+"\t\t   ===");
+    System.out.println("==============================");
+}}
+
+
+
+
+
