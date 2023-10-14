8521aa5facb37814fc63abe199422616289bbb7c
Muhammad Zuhdi <146620601+ZPuh@users.noreply.github.com>
2023-10-14 22:41:22 +0800

    (6/100)
    kata kunci var

---
+++ Day6.java
@@ -0,0 +1,21 @@
+package day6;
+public class Day6 {
+    public static void main(String[] args) {
+    // hari ini saya belajar kata kunci var
+    // Kata Kunci Var
+    var varint = 12;
+    System.out.println(varint);
+    var varlong = 12L;
+    System.out.println(varlong);
+    var varfloat = 1.2F;
+    System.out.println(varfloat);
+    var vardouble = 1.222222;
+    System.out.println(vardouble);
+    var varchar = "@";
+    System.out.println(varchar);
+    var varboolean = false;
+    System.out.println(varboolean);
+    var varstring = "Dua Belas";
+    System.out.println(varstring);
+ 
+}}
