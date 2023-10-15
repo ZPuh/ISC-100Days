f012f5801f81b25bcbd1f6ffe7708f2ffbf0afb6
Muhammad Zuhdi <146620601+ZPuh@users.noreply.github.com>
2023-10-15 20:48:54 +0800

    (7/100)
    operasi matematika (aritmatika)

---
+++ Day7.java
@@ -0,0 +1,21 @@
+package day7;
+public class Day7 {
+    public static void main(String[] args) {
+    // Operasi Matematika
+    // Penjumlahan    ( + )
+    // Pengurangan    ( - )
+    // Perkalian      ( * )
+    // Pembagian      ( / )
+    // Sisa Pembagian ( % )
+    int a , b ;
+    a = 1000;
+    b = 10;
+    System.out.println("=====OPERASI MATEMATIKA=====");
+    System.out.println("===== A + B = "+(a+b)+"     =====");
+    System.out.println("===== A - B = "+(a-b)+"      =====");
+    System.out.println("===== A x B = "+(a*b)+"    =====");
+    System.out.println("===== A : B = "+(a/b)+"      =====");
+    System.out.println("===== A % B = "+(a%b)+"        =====");
+    System.out.println("============================");
+    
+}}
