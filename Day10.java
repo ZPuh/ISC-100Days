7189bb13a1e1e2a78d6eef5d294acc341c3b3435
Muhammad Zuhdi <146620601+ZPuh@users.noreply.github.com>
2023-10-18 20:06:53 +0800

    (10/100)
    belajar operasi perbandingan

---
+++ Day10.java
@@ -0,0 +1,23 @@
+package day10;
+public class Day10 {
+    public static void main(String[] args) {
+        // OPERASI PERBANDINGAN 
+        // Lebih Dari (>)
+        // Kurang Dari (<)
+        // Lebih dari sama dengan (>=)
+        // Kurang dari sama dengan (<=)
+        // Sama dengan (==)
+        // Tidak sama dengan (!=)
+        
+        int nilai1 = 100;
+        int nilai2 = 100;
+        
+        System.out.println(nilai1>nilai2);
+        System.out.println(nilai1<nilai2);
+        System.out.println(nilai1>=nilai2);
+        System.out.println(nilai1<=nilai2);
+        System.out.println(nilai1==nilai2);
+        System.out.println(nilai1!=nilai2);
+     }
+    
+}
