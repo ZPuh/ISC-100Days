77d438c8d145712be2a3cde8a9e422da1aa70516
Muhammad Zuhdi <146620601+ZPuh@users.noreply.github.com>
2023-11-11 22:13:02 +0800

    (34/100)

---
+++ FoorEach.java
@@ -0,0 +1,13 @@
+package fooreach;
+public class FoorEach {
+    public static void main(String[] args) {
+        // For Each
+        
+        String[] nama = {"Adam","Wilson","El Gato","Bill"};
+        
+        for(String i : nama){
+            System.out.println(i);
+        }
+    }
+    
+}
