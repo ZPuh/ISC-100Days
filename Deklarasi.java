4a553675646e9a0abeefb4360ca4d60311bee8b6
Muhammad Zuhdi <146620601+ZPuh@users.noreply.github.com>
2023-11-14 20:14:13 +0800

    (37/100)

---
+++ Deklarasi.java
@@ -0,0 +1,28 @@
+package array;
+public class Deklarasi {
+    public static void main(String[] args) {
+        // Deklarasi Array
+        
+        int[] Genap = {2,4,6,8,10};        
+        System.out.print(Genap[0]+" ");
+        System.out.print(Genap[1]+" ");
+        System.out.print(Genap[2]+" ");
+        System.out.print(Genap[3]+" ");
+        System.out.println(Genap[4]);
+        
+        int[] Ganjil = new int[5];
+        Ganjil [0]= 1;
+        Ganjil [1]= 3;
+        Ganjil [2]= 5;
+        Ganjil [3]= 7;
+        Ganjil [4]= 9;
+        
+        System.out.print(Ganjil [0]+" ");
+        System.out.print(Ganjil [1]+" ");
+        System.out.print(Ganjil [2]+" ");
+        System.out.print(Ganjil [3]+" ");
+        System.out.println(Ganjil [4]);
+        
+    }
+    
+}
