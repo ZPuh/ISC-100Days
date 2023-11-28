76e205b0fcfa8a63089fc50e6a2f16295d205149
Muhammad Zuhdi <146620601+ZPuh@users.noreply.github.com>
2023-11-28 20:44:33 +0800

    (51/100)

--- Arr2D2.java
+++ Arr2D2.java
@@ -1,7 +1,5 @@
 package isc100days.Arrays;
-
 import java.util.Arrays;
-
 public class Arr2D2 {
     public static void main(String[] args) {
         int[][] arrayint ={
@@ -11,5 +9,6 @@ public class Arr2D2 {
         System.out.println(Arrays.toString(arrayint));
         // [[I@6acbcfc0, [I@5f184fc6]
         System.out.println(Arrays.deepToString(arrayint));
+        // [[4, 8, 2], [6, 8, 4]]
     }    
 }
