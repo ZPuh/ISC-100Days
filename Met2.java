ba365c361c1bea2894aa64e0b9860c2060ffa62d
Muhammad Zuhdi <146620601+ZPuh@users.noreply.github.com>
2023-11-30 20:22:44 +0800

    (53/100)
    Void Method

---
+++ Met2.java
@@ -0,0 +1,14 @@
+package isc100days.Method;
+
+public class Met2 {
+
+    public static void main(String[] args) {
+
+        print("Zuhdi");
+    }
+
+    private static void print(String nama) {
+        System.out.println("hy " + nama + " Selamat Datang");
+    }
+
+}
