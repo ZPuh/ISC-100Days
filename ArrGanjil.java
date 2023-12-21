f02c7cf49b06d7d742e5a5e658aa9d2da479623f
Muhammad Zuhdi <146620601+ZPuh@users.noreply.github.com>
2023-12-21 21:57:04 +0800

    (74/100)

--- ArrGanjil.java
+++ ArrGanjil.java
@@ -10,18 +10,19 @@ public class ArrGanjil {
         System.out.print("Masukan Banyak Index : ");
         int index = sc.nextInt();
         int[] bilangan = new int[index];
+        int jumlah = 0;
         System.out.println("Masukkan Angka : ");
         for (int i = 0; i < bilangan.length; i++) {
             System.out.print("Angka -" + (i + 1) + " : ");
             bilangan[i] = sc.nextInt();
         }
-        System.out.println("Angka Ganjil dari Array Tersebut : ");
+        System.out.print("Jumlah Angka Ganjil dari Array Tersebut : ");
         for (int i : bilangan) {
             if (i % 2 == 1) {
-                System.out.print(i + " ");
+                jumlah += i;
             }
         }
-        System.out.println();
+        System.out.println(jumlah);  
     }
 
 }
