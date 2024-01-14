69cbbc5b41d3d0fc7f41773eddbd29f1dcdb90ed
Muhammad Zuhdi <146620601+ZPuh@users.noreply.github.com>
2024-01-14 18:12:32 +0800

    (98/100)

---
+++ RemoveStringSpaces.java
@@ -0,0 +1,23 @@
+package String;
+
+public class RemoveStringSpaces {
+
+    /*
+Tulis fungsi yang menghilangkan spasi dari string, lalu kembalikan string yang dihasilkan.
+
+Contoh:
+
+Input -> Output
+"8 j 8   mBliB8g  imjB8B8  jl  B" -> "8j8mBliB8gimjB8B8jlB"
+"8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd" -> "88Bifk8hB8BB8BBBB888chl8BhBfd"
+"8aaaaa dddd r     " -> "8aaaaaddddr"*/
+    public static void main(String[] args) {
+        System.out.println(noSpace("8 j 8   mBliB8g  imjB8B8  jl  B"));
+        System.out.println(noSpace("8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd"));
+        System.out.println(noSpace("8aaaaa dddd r     "));
+    }
+
+    public static String noSpace(final String x) {
+        return x.replaceAll(" ", "");
+    }
+}
